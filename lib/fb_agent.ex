c46c62267a4262538fd01c7fe097a4f357f91f87
libitx <libitx@protonmail.com>
2019-09-06 16:28:10 +0100

    Implement child_spec

--- lib/fb_agent.ex
+++ lib/fb_agent.ex
@@ -24,6 +24,16 @@ defmodule FBAgent do
   end
 
 
+  @doc false
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :supervisor
+    }
+  end
+
+
   @doc """
   TODOC
   """
