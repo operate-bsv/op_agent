925ee23538ecfc0ea13841b39e325ce8fd493a0b
libitx <libitx@protonmail.com>
2020-02-05 20:42:19 +0000

    Beta version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Operate.MixProject do
   def project do
     [
       app: :operate,
-      version: "0.1.0-beta.7",
+      version: "0.1.0-beta.8",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
