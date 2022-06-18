49564f7430bccab44f0e396c188725fa3e8182ec
digitsu <digitsu@gmail.com>
2022-06-18 19:50:22 +0900

    Fixed old call to BSV.Crypto in ConCache

--- lib/operate/cache/con_cache.ex
+++ lib/operate/cache/con_cache.ex
@@ -19,50 +19,53 @@ defmodule Operate.Cache.ConCache do
   """
   use Operate.Cache
 
-
   def fetch_tx(txid, _options \\ [], {adapter, adapter_opts}) do
-    key = "t:#{ txid }"
+    key = "t:#{txid}"
+
     ConCache.fetch_or_store(:operate, key, fn ->
       adapter.fetch_tx(txid, adapter_opts)
     end)
   end
 
-
   def fetch_tx_by(query, _options \\ [], {adapter, adapter_opts})
-    when is_map(query)
-  do
-    hash = query
-    |> Jason.encode!
-    |> BSV.Crypto.Hash.sha256
-    key = "t:#{ hash }"
+      when is_map(query) do
+    hash =
+      query
+      |> Jason.encode!()
+      |> BSV.Hash.sha256()
+
+    key = "t:#{hash}"
+
     ConCache.fetch_or_store(:operate, key, fn ->
       adapter.fetch_tx_by(query, adapter_opts)
     end)
   end
 
-
   def fetch_ops(refs, _options \\ [], {adapter, adapter_opts}) do
-    cached_ops = refs
-    |> Enum.map(& ConCache.get(:operate, "p:#{&1}"))
-    |> Enum.reject(& is_nil(&1))
+    cached_ops =
+      refs
+      |> Enum.map(&ConCache.get(:operate, "p:#{&1}"))
+      |> Enum.reject(&is_nil(&1))
 
-    cached_refs = cached_ops
-    |> Enum.map(& &1.ref)
+    cached_refs =
+      cached_ops
+      |> Enum.map(& &1.ref)
 
-    uncached_refs = refs
-    |> Enum.reject(& &1 in cached_refs)
+    uncached_refs =
+      refs
+      |> Enum.reject(&(&1 in cached_refs))
 
-    uncached_ops = case length(uncached_refs) do
-      0 -> {:ok, []}
-      _ -> adapter.fetch_ops(uncached_refs, adapter_opts)
-    end
+    uncached_ops =
+      case length(uncached_refs) do
+        0 -> {:ok, []}
+        _ -> adapter.fetch_ops(uncached_refs, adapter_opts)
+      end
 
     with {:ok, ops} <- uncached_ops do
-      Enum.each(ops, & ConCache.put(:operate, "p:#{ &1.ref }", &1))
+      Enum.each(ops, &ConCache.put(:operate, "p:#{&1.ref}", &1))
       {:ok, Enum.concat(cached_ops, ops)}
     else
       error -> error
     end
   end
-
 end
