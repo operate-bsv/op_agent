72b9b1c8655c2860b5f5e340afae876cb3a62ee6
digitsu <digitsu@gmail.com>
2022-06-18 16:09:17 +0900

    Upgrading the lib to use the BSV 2.x crypto library, as to be compliant with erlang OTP 25

--- lib/operate/util.ex
+++ lib/operate/util.ex
@@ -3,7 +3,6 @@ defmodule Operate.Util do
   A collection of commonly used helper functions.
   """
 
-  
   @doc """
   Converts the given map into a struct. Must be used with a struct that has
   previously been serialised into a map with stringified keys and still contains
@@ -16,17 +15,19 @@ defmodule Operate.Util do
       true
   """
   @spec restruct(map) :: struct
-  
+
   def restruct(%{"__struct__" => mod_str} = map) do
     mod = String.to_atom(mod_str)
 
-    keys = Map.keys(struct(mod))
-    |> Enum.filter(& &1 != :__struct__)
+    keys =
+      Map.keys(struct(mod))
+      |> Enum.filter(&(&1 != :__struct__))
 
-    attrs = for key <- keys do
-      val = Map.get(map, key) || Map.get(map, to_string(key))
-      {key, restruct(val)}
-    end
+    attrs =
+      for key <- keys do
+        val = Map.get(map, key) || Map.get(map, to_string(key))
+        {key, restruct(val)}
+      end
 
     struct(mod, attrs)
   end
@@ -36,4 +37,69 @@ defmodule Operate.Util do
 
   def restruct(val), do: val
 
+  @doc """
+  converts a lua table back to elixir struct
+
+  tuplelist = [
+    {"__struct__", "Elixir.BSV.PrivKey"},
+    {"compressed", true},
+    {"d",
+     <<152, 12, 110, 246, 195, 210, 240, 228, 109, 35, 145, 158, 135, 48, 12, 10,
+       115, 251, 123, 203, 15, 54, 26, 75, 129, 185, 10, 43, 223, 5, 210, 45>>}
+  ]
+
+  output:
+    %BSV.PubKey{
+    compressed: true,
+    point: %Curvy.Point{
+      x: 82088944861703957177930580775501328651185625409420569396329737753348304696900,
+      y: 44719512211966310551039175512542915384530845949753535174026330840076849401334
+    }
+    }
+
+  """
+  def lua_table_to_struct(table) do
+    restruct(tuplelist_to_map(table))
+  end
+
+  defp tuplelist_to_map(tuplelist) do
+    a_struct =
+      Enum.reduce(tuplelist, [], fn elem, acc ->
+        case elem do
+          {k, maybe_struct} when is_list(maybe_struct) ->
+            acc ++ [{k, tuplelist_to_map(maybe_struct)}]
+
+          {a, b} ->
+            acc ++ [{a, b}]
+
+          _ ->
+            acc
+        end
+      end)
+
+    Enum.into(a_struct, %{})
+  end
+
+  @doc """
+  a list in elixir such as this:
+
+  original = [
+    <<116, 66, 140, 64, 43, 144, 54, 197, 38, 126, 89>>,
+    <<255, 39, 72, 204, 45, 248, 185, 27, 36, 195, 128, 152, 183, 220, 229, 31>>
+  ]
+
+  passed into lua, turns into a table and in an Elixir fn call, looks like this:
+
+  table = [
+    {1, <<116, 66, 140, 64, 43, 144, 54, 197, 38, 126, 89>>},
+    {2,
+     <<255, 39, 72, 204, 45, 248, 185, 27, 36, 195, 128, 152, 183, 220, 229, 31>>}
+  ]
+
+    This fn changes it back.
+
+  """
+  def lua_table_to_list(table) do
+    Enum.into(table, [], fn {_k, v} -> v end)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -4,9 +4,9 @@ defmodule Operate.MixProject do
   def project do
     [
       app: :operate,
-      version: "0.1.0-beta.15",
+      version: "0.1.0-beta.16",
       elixir: "~> 1.9",
-      elixirc_paths: elixirc_paths(Mix.env),
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "Operate",
@@ -15,7 +15,7 @@ defmodule Operate.MixProject do
       docs: [
         main: "Operate",
         groups_for_modules: [
-          "Extensions": [
+          Extensions: [
             Operate.VM.Extension,
             Operate.VM.Extension.Agent,
             Operate.VM.Extension.Base,
@@ -24,16 +24,16 @@ defmodule Operate.MixProject do
             Operate.VM.Extension.JSON,
             Operate.VM.Extension.String
           ],
-          "Adapters": [
+          Adapters: [
             Operate.Adapter,
             Operate.Adapter.Bob,
             Operate.Adapter.OpApi
           ],
-          "Caches": [
+          Caches: [
             Operate.Cache,
             Operate.Cache.ConCache,
             Operate.Cache.NoCache
-          ],
+          ]
         ]
       ],
       package: [
@@ -58,22 +58,23 @@ defmodule Operate.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:bsv, "~> 0.3"},
+      {:bsv, "~> 2.1"},
       {:con_cache, "~> 0.14"},
       {:ex_doc, "~> 0.22", only: [:dev, :publish], runtime: false},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.1", only: :test},
       {:terminus, "~> 0.1"},
       {:tesla, "~> 1.3"},
-      luerl_dep(Mix.env)
+      {:ex_crypto, github: "ntrepid8/ex_crypto", branch: "master"},
+      luerl_dep(Mix.env())
     ]
   end
 
   defp luerl_dep(:publish), do: {:luerl, "~> 0.4"}
+
   defp luerl_dep(_),
     do: {:luerl, github: "rvirding/luerl", branch: "develop"}
 
   defp elixirc_paths(:test), do: elixirc_paths(:default) ++ ["test/support"]
   defp elixirc_paths(_), do: ["lib"]
-
 end
